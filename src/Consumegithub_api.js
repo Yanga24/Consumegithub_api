0096be6ea36149ae805eed479413eb62f953c3da
Yanga24 <ntombikanzi1@gmail.com>
2020-05-19 10:57:14 +0200

    first commit

---
+++ src/Consumegithub_api.js
@@ -0,0 +1,20 @@
+function Api ( reporitory_name, start_date, end_date){
+
+
+}
+
+'reporitory_name' = API
+'start date' = 10/02/2020
+'end date' = 10/04/2020
+
+then (function (respond){
+return Response.json();
+
+});
+then (function (myJson){
+console.log('error: "+ error');
+console.log('repository_name, start_date, end_date');
+
+});
+
+module.exports = {Consumegithub_api}
